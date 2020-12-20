ad8c84f4b76e81fb6192c5a11fcf126544a65339
BashirSim <zi10zo95@gmail.com>
2020-12-20 16:17:09 +0300

    Added new feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -120,6 +120,9 @@ async function controlAddRecipe(newRecipe) {
   }
 };
 
+function newFeature() {
+  console.log('Welcom to the application!');
+}
 
 function init() {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -129,7 +132,7 @@ function init() {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
- console.log('hi'); 
+  newFeature();
 };
 
 init();
