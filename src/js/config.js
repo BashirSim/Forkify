15107c69f5b13155e676bf9a7053c48c6a57222d
BashirSim <zi10zo95@gmail.com>
2020-12-20 15:59:08 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10; 
+export const RES_PER_PAGE = 10;
+export const KEY = '3558a6f0-19bb-4e09-9b8e-77a888f30a75';
+export const MODAL_CLOSE_SEC = 2500;
---
+++ src/js/controller.js
@@ -0,0 +1,136 @@
+import * as model from './model.js';
+import { MODAL_CLOSE_SEC } from './config.js';
+import recipeView from './Views/recipeView.js';
+import searchView from './Views/searchView.js';
+import resultsView from './Views/resultsView.js';
+import paginationView from './Views/paginationView.js';
+import bookmarksView from './Views/bookmarksView.js';
+import addRecipeView from './Views/addRecipeView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+async function controlRecipes() {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 0) Update results view to mark selected search result
+    // resultsView.update(model.getSearchResultsPage());
+    resultsView.render(model.getSearchResultsPage());
+    bookmarksView.render(model.state.bookmarks);
+    
+    // 1) Loading recipe
+    await model.loadRecipe(id);
+
+    //2) Rendering recipe
+    recipeView.render(model.state.recipe);
+
+  } catch (err) {
+    recipeView.renderError();
+  }
+};
+
+async function controlSearchResults() {
+  try {
+
+    resultsView.renderSpinner();
+
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    // 3) Render results
+    resultsView.render(model.getSearchResultsPage());
+
+    // 4) Render intial pagination button
+    paginationView.render(model.state.search);
+
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+function controlPagination(goToPage) {
+   // 1) Render new results
+   resultsView.render(model.getSearchResultsPage(goToPage));
+
+   // 2) Render new pagination button
+   paginationView.render(model.state.search);
+}
+
+function controlServings(newServings) {
+  // Update the recipe serving (ins state)
+  model.updateServings(newServings);
+
+  // Update recipe view
+   recipeView.render(model.state.recipe);
+  // recipeView.update(model.state.recipe);
+};
+
+function controlAddBookmark() {
+  // 1) Add/remove bookmark
+  if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
+  else model.deleteBookmark(model.state.recipe.id);
+
+  // 2) Update recipe view
+  recipeView.render(model.state.recipe);
+
+  // 3) Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
+}
+
+function controlBookmarks() {
+  bookmarksView.render(model.state.bookmarks);
+}
+
+async function controlAddRecipe(newRecipe) {
+  try {
+    // Show loading spinner
+    addRecipeView.renderSpinner();
+
+    // Upload the new recipe data
+    await model.uploadRecipe(newRecipe);
+    console.log(model.state.recipe);
+
+    // Render recipe
+    recipeView.render(model.state.recipe);
+
+    // Succsess message 
+    addRecipeView.renderMessage();
+
+    // Render bookmarkview
+    bookmarksView.render(model.state.bookmarks);
+
+    // Change the ID in URL
+    window.history.pushState(null, '', `#${model.state.recipe.id}`);
+
+    // Close form window
+    setTimeout(function () {
+      addRecipeView.toggleWindow()
+    }, MODAL_CLOSE_SEC * 1000);
+  } catch (err) {
+    console.error('****', err);
+    addRecipeView.renderError(err.message);
+  }
+};
+
+
+function init() {
+  bookmarksView.addHandlerRender(controlBookmarks);
+  recipeView.addHandlerRender(controlRecipes);
+  recipeView.addhandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddbookmark(controlAddBookmark);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
+ console.log('hi'); 
+};
+
+init();
+
